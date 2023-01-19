bef6f8e0f95f35fa1bf0f7a12337f34b78db4983
An Huynh <110524240+anhuynh1234@users.noreply.github.com>
2023-01-19 15:42:24 -0700

    changes

--- Schedule_reminder/schedule.py
+++ Schedule_reminder/schedule.py
@@ -4,14 +4,22 @@ class schedule:
     def __init__(self):
         self.name = []
         self.date = []
-        self.time = []
+        self.course = dict()
         self.list_all = []
     
     def add_course(self, name):
         self.list_all.append(name)
     
-    def add_class(self, name):
-        self.name.append(list(name)[0])
+    # def add_class(self, name):
+    #     self.name.append(list(name)[0])
+    
+    def add_time(self, course):
+        tokens = list(course.split())
+        course_name = tokens[0]
+        time = tokens[1]
+        days = [tokens[i:i+3] for i in range(0, len(tokens[2]), 3)]
+        dates = [x + " " + time for x in days]
+        self.course[course_name] = dates
 
     def getlist(self):
         return self.list_all
